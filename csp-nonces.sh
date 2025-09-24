a295272e1d9a58d90cd80fbafdcda61eca3e185f
Serhii Haras <admin@haras.space>
2025-09-24 18:55:56 +0200

    refactor

--- csp-nonces.sh
+++ csp-nonces.sh
@@ -5,7 +5,8 @@ NONCE="CSP_NONCE_INJECTION"
 INDEX_HTML="./dist/ng-test-project/browser/index.html"
 
 if [ -f "$INDEX_HTML" ]; then
-  sed -i '' "s/<link rel=\"modulepreload\"/<link rel=\"modulepreload\" nonce=\"$NONCE\"/g" "$INDEX_HTML"
+  sed -i "s/<link rel=\"modulepreload\"/<link rel=\"modulepreload\" nonce=\"$NONCE\"/g" "$INDEX_HTML"
 else
+  echo "index.html not found at $INDEX_HTML"
   exit 1
 fi
