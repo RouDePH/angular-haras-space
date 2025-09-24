0fcacc8b1d386352b3c6794d69d3d7061e9b03e1
Serhii Haras <admin@haras.space>
2025-09-24 20:36:40 +0200

    refactor

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,52 @@
 import {
   ApplicationConfig,
   CSP_NONCE,
+  importProvidersFrom,
   provideBrowserGlobalErrorListeners,
   provideZonelessChangeDetection,
 } from '@angular/core';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
 
 import { routes } from './app.routes';
+import { DBConfig, NgxIndexedDBModule } from 'ngx-indexed-db';
+
+export function migrationFactory() {
+  // The animal table was added with version 2 but none of the existing tables or data needed
+  // to be modified so a migrator for that version is not included.
+  return {
+    1: (db: IDBDatabase, transaction: IDBTransaction) => {
+      const store = transaction.objectStore('people');
+      store.createIndex('country', 'country', { unique: false });
+    },
+    3: (db: IDBDatabase, transaction: IDBTransaction) => {
+      const store = transaction.objectStore('people');
+      store.createIndex('age', 'age', { unique: false });
+    },
+  };
+}
+
+const dbConfig: DBConfig = {
+  name: 'MyDb',
+  version: 3,
+  objectStoresMeta: [
+    {
+      store: 'people',
+      storeConfig: { keyPath: 'id', autoIncrement: true },
+      storeSchema: [
+        { name: 'name', keypath: 'name', options: { unique: false } },
+        { name: 'email', keypath: 'email', options: { unique: false } },
+      ],
+    },
+    {
+      // animals added in version 2
+      store: 'animals',
+      storeConfig: { keyPath: 'id', autoIncrement: true },
+      storeSchema: [{ name: 'name', keypath: 'name', options: { unique: true } }],
+    },
+  ],
+  // provide the migration factory to the DBConfig
+  migrationFactory,
+};
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -14,5 +54,6 @@ export const appConfig: ApplicationConfig = {
     provideZonelessChangeDetection(),
     provideRouter(routes, withComponentInputBinding()),
     { provide: CSP_NONCE, useValue: 'CSP_NONCE_INJECTION' },
+    importProvidersFrom(NgxIndexedDBModule.forRoot(dbConfig))
   ],
 };
--- src/app/pages/404/404-component.ts
+++ src/app/pages/404/404-component.ts
@@ -1,28 +1,58 @@
-import { Component, computed, Input, } from '@angular/core';
+import { Component, computed, Input, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { LocaleLink } from '../../../locale/locale.link';
 import { PATH } from '../../app.paths';
+import { NgxIndexedDBService } from 'ngx-indexed-db';
 
 @Component({
   selector: 'app-404-component',
   imports: [RouterLink, LocaleLink],
   templateUrl: './404-component.html',
   styleUrl: './404-component.css',
+  providers: [NgxIndexedDBService],
   standalone: true,
 })
-export class NotFoundComponent {
+export class NotFoundComponent implements OnInit {
   @Input() resolveFoo?: string; // My resolved data
   @Input() isSomething?: boolean; // true
   @Input() todoId?: string; // 1
   @Input() searchTerm?: string; // angular
   @Input() locale?: string; // angular
 
-  constructor(){}
-
   protected readonly message = 'PAGE NOT FOUND';
   protected readonly PATH = PATH;
 
-  hasUnsavedChanges = () => true
+  hasUnsavedChanges = () => true;
 
   getMessage = computed<string>(() => `Message: ${this.message}`);
+
+  name = 'Angular';
+  people: any[] = [];
+  constructor(private dbService: NgxIndexedDBService) {
+    this.dbService.clear('people').subscribe((successDeleted) => {
+      console.log('success? ', successDeleted);
+    });
+
+    this.dbService
+      .bulkAdd('people', [
+        {
+          name: `charles number ${Math.random() * 10}`,
+          email: `email number ${Math.random() * 10}`,
+        },
+        {
+          name: `charles number ${Math.random() * 10}`,
+          email: `email number ${Math.random() * 10}`,
+        },
+      ])
+      .subscribe((result) => {
+        console.log('result: ', result);
+      });
+  }
+
+  ngOnInit() {
+    this.dbService.getAll('people').subscribe((result: any) => {
+      console.log('results: ', result);
+      this.people = result;
+    });
+  }
 }
