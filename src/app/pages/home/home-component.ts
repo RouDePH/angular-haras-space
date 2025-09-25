c274285e878ba7c9e75949dbea5bbdd11bf9a02c
Serhii Haras <admin@haras.space>
2025-09-25 12:44:13 +0200

    feat: added push logic

--- src/app/pages/home/home-component.ts
+++ src/app/pages/home/home-component.ts
@@ -5,6 +5,7 @@ import { UserContext } from '../../user.context';
 import { PATH } from '../../app.paths';
 import { TranslatePipe } from '../../../locale/translate.pipe';
 import { JsonPipe } from '@angular/common';
+import { PushNotificationService } from '../../notifications.service';
 
 @Component({
   selector: 'app-home-component',
@@ -22,7 +23,16 @@ export class HomeComponent {
 
   protected readonly message = 'This is a home component';
   protected readonly PATH = PATH;
-  constructor(public userCtx: UserContext) {}
+  constructor(public userCtx: UserContext, private pushService: PushNotificationService) {}
+
+  subscribe() {
+    this.pushService.register();
+  }
+
+  send() {
+    this.pushService.notify('Привет!', 'Это тестовое уведомление', '/');
+  }
+
 
   getMessage = computed<string>(
     () => `Message: ${this.message}, Input: ${JSON.stringify(this.userCtx.user())}`
